fca9d5efa2edb57d21dc1b93e9cfa724d562190d
Eric <edhavey@null.net>
2021-09-21 22:03:13 -0500

    commit tree f63a38425eb6e16d6561bf2eb4839faf684cc7fd

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    const temp = [];
+    title.split(' ').forEach(word => temp.push(word[0].toUpperCase() + word.slice(1)))
+    return temp.join(' ');
+  })
 }
+
+
